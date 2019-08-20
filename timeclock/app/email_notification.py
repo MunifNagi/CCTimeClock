11f7d7b1d5a467a6bec32bfcd45eabad95c67c88
Munif Nagi <mnagi@records.nyc.gov>
2019-08-20 12:52:47 -0400

    Revert "fixed failing pytest"
    This reverts commit b6ddef1bb7ac24ddf186125913dc3280a2828618.

--- timeclock/app/email_notification.py
+++ timeclock/app/email_notification.py
@@ -5,6 +5,7 @@ from flask_mail import Message
 
 from app.extensions import mail
 
+
 def send_async_email(app, msg):
     """
     Sends asynchronous e-mails, allowing the server to avoid delay
@@ -28,7 +29,7 @@ def send_email(to, subject, template, **kwargs):
     """
     app = current_app._get_current_object()
     msg = Message('TimeClock' + ' ' + subject,
-                  sender='Records Timeclock <RTimeclock@records.nyc.gov>', recipients=[to])
+                  sender=app.config['MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_async_email, args=[app, msg])
